2076096637332195e99d96caf58168dd49682464
zhakhalov <zhakhalov@gmail.com>
2015-09-28 01:44:32 +0300

    models added

---
+++ Gallery.Models/GalleryContext.cs
@@ -0,0 +1,17 @@
+﻿using Gallery.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery.Models
+{
+    public class GalleryContext : DbContext
+    {
+        public GalleryContext(string connectionString) : base(connectionString) { }
+
+        public DbSet<User> Users;
+    }
+}
--- Gallery.WebUI/Controllers/HomeController.cs
+++ Gallery.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Gallery.Models;
+using Gallery.Models.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;

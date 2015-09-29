a5e4a7e791766a53d01184487470b83579f3d081
zhakhalov <zhakhalov@gmail.com>
2015-09-30 01:29:18 +0300

    owin startup added

---
+++ Gallery.WebUI/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Diagnostics;
+
+[assembly: OwinStartup(typeof(Gallery.WebUI.Startup))]
+
+namespace Gallery.WebUI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            Debug.WriteLine("Startup");
+        }
+    }
+}
